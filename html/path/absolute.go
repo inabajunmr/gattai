4d2209d65a3ca2e0a923e505e1ab5855a80b57b3
inabajunmr <inabajun.for.regi@gmail.com>
2018-11-09 01:38:01 +0900

    wip

--- html/path/absolute.go
+++ html/path/absolute.go
@@ -1,7 +1,7 @@
 package path
 
 // ModifyToAbsoluteURL from relative path in html string
-func ModifyToAbsoluteURL(html string, host string) string {
+func ModifyToAbsoluteURL(html string, url string) string {
 	// load html
 	// get elements with url
 	// modify all
--- html/path/absolute_test.go
+++ html/path/absolute_test.go
@@ -4,5 +4,18 @@ import "testing"
 
 func TestModifyToAbsoluteURL(t *testing.T) {
 	// prepare sample html
-	TestModifyToAbsoluteURL("", "")
+	ModifyToAbsoluteURL(`<html>
+							<body>
+								<a href='/test1'>Test</a>
+								<a href='/test2'>Test</a>
+								<img src='/test3'>
+								<img src='./test4'>
+								<img src='./test5'>
+								<img src='../test6'>
+								<img src='../../test7'>
+								<img src='test8'>
+								<img src='test/test9'>
+								<img src='http://example.com/test10'>
+							</body>
+						</html>`, "")
 }
